8b1c8dca3967f6725374509e1b3c5c4a1c909f19
hermans <ysf.hermanto@gmail.com>
2016-02-07 22:48:54 +0700

    Update db.php

--- db.php
+++ db.php
@@ -5,10 +5,12 @@
 
 class db
 {
+    // connect to database
     public function connectToDb(){
         
     }
     
+    // close database connection
     public function closeDb(){
         
     }
